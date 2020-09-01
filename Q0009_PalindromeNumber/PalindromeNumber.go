8c6e0b71ea98a35e22c0898e87f02630c8cf23ed
sshim from desktop <mechurak@gmail.com>
2020-09-02 00:19:13 +0900

    Solve q9

--- Q0009_PalindromeNumber/PalindromeNumber.go
+++ Q0009_PalindromeNumber/PalindromeNumber.go
@@ -1,5 +1,22 @@
 package q0009
 
 func isPalindrome(x int) bool {
-    return false
+	if x < 0 {
+		return false
+	}
+
+	var digits []int
+	n := x
+	for n > 0 {
+		l := n % 10
+		digits = append(digits, l)
+		n = n / 10
+	}
+	size := len(digits)
+	for i := 0; i < size/2; i++ {
+		if digits[i] != digits[size-1-i] {
+			return false
+		}
+	}
+	return true
 }
